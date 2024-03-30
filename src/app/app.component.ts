62e8e47a851354d0490a73dd99e4d8f7ed4368fe
jacques-myburgh-pepla <jacquesm@pepla.co.za>
2024-03-31 01:12:33 +0200

    Add Angular Material dependencies and implement dark theme support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-portfolio';
+  title = 'My Portfolio';
+  isDarkTheme: boolean = false;
+
+  @HostBinding('class') className = '';
+
+  constructor() {
+    if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
+      this.isDarkTheme = localStorage.getItem('theme') === 'dark' ? true : false;
+      this.className = this.isDarkTheme ? 'dark-theme' : 'light-theme';
+    } else {
+      this.className = 'light-theme';
+    }
+  }
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+    this.className = this.isDarkTheme ? 'dark-theme' : 'light-theme';
+
+    if (typeof localStorage !== 'undefined') {
+      localStorage.setItem('theme', this.isDarkTheme ? 'dark' : 'light');
+    }
+  }
 }
